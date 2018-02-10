35a6bab0f09737b959f6d627efb3478373a39c4b
Richard <feuer@feuerundbenzin.tk>
2018-02-10 01:35:02 +0100

    add static content

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -6,8 +6,8 @@ import alsaaudio
 from flask import Flask, render_template, send_file
 
 mypath = os.path.dirname(os.path.realpath(__file__))
-template_dir = os.path.realpath(mypath + '/wwwroot')
-static_folder = os.path.realpath(mypath + '/wwwroot')
+template_dir = os.path.realpath(mypath + '/template')
+static_folder = os.path.realpath(mypath + '/resroot')
 app = Flask(__name__,template_folder=template_dir,static_url_path='/res',static_folder=static_folder)
 
 @app.route('/')
@@ -20,9 +20,7 @@ def sound(id):
 	m.setvolume(id)
     	return 'OK' 
 
-@app.route('/res/<path:path>')
-def send_js(path):
-    return send_from_directory('js', path)
+
 
 
 if __name__ == '__main__':
